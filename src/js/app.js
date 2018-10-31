e7e52098b2e60975f5e26e70ffebb78f62194938
Hamza <lelouch.hamza@gmail.com>
2018-10-31 03:40:51 +0000

    Separate service worker registration into a module

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import idb from 'idb';
-import Snackbars from '@salahhamza/snackbars';
 import {insertAfter, handleErrors, iterObj} from './utils';
+import ServiceWorkerRegistration from './swRegistration';
 
 /**
  *	Opens IDB database and creates object stores
@@ -25,8 +25,6 @@ export default class App {
 		this._addedConversions = [];
 		this._loadingCards = [];
 		this._messageShown = true;
-
-		this.snackbars = new Snackbars(document.body, true);
 	}
 
 	/****************************************
@@ -315,85 +313,11 @@ export default class App {
 	}
 
 	/**
-	 * Registers service workers
-	 */
-	_registerServiceWorker() {
-		if(!navigator.serviceWorker) return;
-
-		navigator.serviceWorker.register('./sw.js')
-		.then((reg) => {
-			if (!navigator.serviceWorker.controller) {
-				this.snackbars.show({
-					name: 'swRegistered',
-					message: 'Service Worker installed! Pages you view are cached for offline use.',
-					duration: 4500
-				});
-				return;
-			}
-
-			if (reg.waiting) {
-				this._updateReady(reg.waiting);
-				return;
-			}
-
-			if (reg.installing) {
-				this._trackInstalling(reg.installing);
-				return;
-			}
-
-			reg.addEventListener('updatefound', () => {
-				this._trackInstalling(reg.installing);
-			});
-
-		}).catch(() => {
-			console.log('Sw registeration failed');
-			/* setting a retry function to retry sw registration */
-			let attempts = 1;
-			(function retry() {
-				attempts *= 2;
-				setTimeout(this._registerServiceWorker, attempts * 60 * 1000);
-			}());
-		});
-
-		// Ensure refresh is only called once.
-		// This works around a bug in "force update on reload".
-		let refreshing;
-		navigator.serviceWorker.addEventListener('controllerchange', () => {
-			if (refreshing) return;
-			window.location.reload();
-			refreshing = true;
-		});
-	}
-
-	_trackInstalling (worker) {
-		worker.addEventListener('statechange', ()  => {
-			if (worker.state == 'installed') {
-				this._updateReady(worker);
-			}
-		});
-	};
-
-	_updateReady(worker) {
-		this.snackbars.show({
-			message: 'New version available! Refresh to update.',
-			name: 'update',
-			actions: [{
-				name: 'refresh',
-				handler() {
-					console.log('skip waiting and refreshing')
-					worker.postMessage({action: 'skipWaiting'});
-				}
-			}, {
-				name: 'dismiss'
-			}]
-		});
-	};
-
-	/**
 	 * App initializer
 	 */
-	init(){
-		// this._registerServiceWorker();
+	init() {
+		// initializing service worker registration
+		new ServiceWorkerRegistration().init();
 
 		/* Getting currency options */
 		this._getCurrencies();
