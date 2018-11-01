e78d49f9007fbb03ea864f0385322a76aad0ce1b
Hamza <lelouch.hamza@gmail.com>
2018-11-01 20:46:55 +0000

    Remove unused utility functions

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,24 +1,17 @@
 /**
+ * put object properties to array
  *
- * @param {Object} obj - object to turn into an iterable
+ * @param {Object} obj - object to turn to array
  */
-export function* iterObj(obj = {}) {
-	for (let prop of Object.keys(obj)){
-		if(obj.hasOwnProperty(prop)){
-			yield obj[prop];
-		}
-	}
-}
-
-
-/**
- * handles errors of fetch reponse
- * @param {Object} response - response object to handle
- */
-export const handleErrors = (response) => {
-	if (!response.ok) throw Error(response.statusText);
-	return response.json();
-}
+export const objToArray = (obj = {}) => {
+  const arr = [];
+  for (let prop of Object.keys(obj)){
+    if(obj.hasOwnProperty(prop)){
+      arr.push(obj[prop]);
+    }
+  }
+  return arr;
+};
 
 /**
  * Insert newNode after referenceNode
@@ -26,34 +19,15 @@ export const handleErrors = (response) => {
  * @param {Object} referenceNode - Dom node to insert after
  */
 export const insertAfter = (newNode, referenceNode) => {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-
-/**
- *
- * @param {String} name - Database name
- * @param {Number} version - Database version
- * @param {Array.<Object>} stores - array of objectstores config objects
- * @return {Promise<Object>} idb promise
- */
-export const openDatabase = (name, version, stores = []) => {
-	return idb.open(name, version, (upgradeDB) => {
-		console.log(`Opened database: ${name}`);
-		for(conststore of stores) {
-			upgradeDB.createObjectStore(store.name, {
-				keyPath: store.keyPath
-			});
-		}
-	});
-}
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+};
 
 /**
  * remove all children of given DOM node
  * @param {Object} node - targteted DOM node
  */
 export const removeChildren = (node) => {
-	while (node.firstChild) {
+  while (node.firstChild) {
     node.removeChild(node.firstChild);
-	}
-}
+  }
+};
