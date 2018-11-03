d935d238fa9b24cfb606c8d746fdd0ebf2e55078
Hamza <lelouch.hamza@gmail.com>
2018-11-03 18:02:47 +0000

    Register service worker on window load event

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,27 @@
+import '@babel/polyfill';
 import App from './app';
 import { insertAfter } from './utils';
 import moneyDataURI from './../images/money.png';
 import './../css/style.css';
 import '@salahhamza/snackbars/lib/snackbar.css';
+import ServiceWorkerRegistration from './swRegistration';
 
 window.addEventListener('DOMContentLoaded', function(){
-	(function(/* Inserting inline brand image */){
-		const brandImage = document.createElement('img');
-		brandImage.src = moneyDataURI;
-		brandImage.alt = 'Currency Converter';
-		const brandName = document.querySelector('.headbar h1');
-		insertAfter(brandImage, brandName);
-	})();
-	const app = new App();
-	app.init();
+
+  ((/* Inserting inline brand image */) => {
+    const brandImage = document.createElement('img');
+    brandImage.src = moneyDataURI;
+    brandImage.alt = 'Currency Converter';
+    const brandName = document.querySelector('.headbar h1');
+    insertAfter(brandImage, brandName);
+  })();
+
+  // initilize app
+  const app = new App();
+  app.init();
+});
+
+window.addEventListener('load', () => {
+  const swRegisterer = new ServiceWorkerRegistration();
+  swRegisterer.registerServiceWorker();
 });
