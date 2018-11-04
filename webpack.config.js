028ac8d06799b43a5a948ca563708efa8c0b4a07
Hamza <lelouch.hamza@gmail.com>
2018-11-04 20:51:22 +0000

    chore: Add javascript linting using eslint

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint-loader',
+      }, {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader'
